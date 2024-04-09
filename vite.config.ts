1a08e4be29427b004356039a20a9271884001585
su <su>
2024-04-09 06:16:28 +0000

    docs

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,14 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/bunvite/',
   plugins: [react()],
   server: {
     host: '0.0.0.0',
     port: 3000
+  },
+  build: {
+    outDir: 'docs',
+
   }
 })
